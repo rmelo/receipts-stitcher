854fc067ec64d0714d04582da253dd063b39912e
Rodrigo Melo <rmelo@Rodrigos-MacBook-Pro.local>
2019-07-04 17:39:05 -0300

    Adds method decorators Adds useful functions Handle with subprocess exception

--- app.py
+++ app.py
@@ -6,30 +6,53 @@ import os
 import uuid
 import glob
 import subprocess
+from functools import wraps
 
 app = Flask(__name__)
-UPLOAD_PATH='./uploads'
+UPLOAD_PATH = './uploads'
 
 print('Starting server...')
 
+
+def response_json(data, status=200):
+    return Response(json.dumps(data), status, mimetype='application/json')
+
+
+def response_error(message, status=400):
+    return response_json({'message': message}, status)
+
+def upload_folder_path(upload_id):
+    return os.path.join(UPLOAD_PATH, upload_id)
+
+def upload_exists(fn):
+    @wraps(fn)
+    def decorator_function(*args, **kwargs):
+
+        upload_id = kwargs['upload_id']
+        folder_path = upload_folder_path(upload_id)
+
+        if(os.path.exists(folder_path) == False):
+            return response_error(f'There\'s no upload with id ${upload_id}')
+
+        return fn(*args, **kwargs)
+    return decorator_function
+
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Stitcher API"
 
 
 @app.route('/uploads', methods=['POST'])
 def create_upload():
 
     upload_id = uuid.uuid4().hex
-    os.makedirs(os.path.join(UPLOAD_PATH, upload_id))
+    os.makedirs(upload_folder_path(upload_id))
 
-    data = {
-        'upload_id': upload_id
-    }
+    data = {'upload_id': upload_id}
 
-    js = json.dumps(data)
+    resp = response_json(data, 201)
 
-    resp = Response(js, status=201, mimetype='application/json')
     resp.headers['Link'] = url_for(
         'upload', upload_id=upload_id, _external=True)
 
@@ -39,19 +62,25 @@ def create_upload():
 @app.route('/uploads', methods=['GET'])
 def get_uploads():
 
-    folders = [os.path.basename(f) for f in glob.glob(os.path.join(UPLOAD_PATH, '**'))]
+    folders = [os.path.basename(f) for f in glob.glob(
+        os.path.join(UPLOAD_PATH, '**'))]
 
-    return Response(json.dumps(folders), status=200, mimetype='application/json')
+    data = [{'upload_id': f, 'link': url_for(
+        'upload', upload_id=f, _external=True)} for f in folders]
+
+    return response_json(data)
 
 
 @app.route('/uploads/<string:upload_id>', methods=['POST'])
+@upload_exists
 def upload(upload_id):
 
-    folder_path = os.path.join(UPLOAD_PATH, upload_id)
-    if(os.path.exists(folder_path) == False):
-        return 'Upload doesn\'t exists', 400
+    folder_path = upload_folder_path(upload_id)
 
     uploaded_files = request.files.getlist('file[]')
+
+    if(len(uploaded_files) == 0):
+        return response_json({'message': 'No file found'})
 
     for file in uploaded_files:
         file_path = os.path.join(folder_path, file.filename)
@@ -59,12 +88,12 @@ def upload(upload_id):
 
     return '', 201
 
+
 @app.route('/uploads/<string:upload_id>/stitch', methods=['POST'])
+@upload_exists
 def stitch(upload_id):
 
     folder_path = os.path.join(UPLOAD_PATH, upload_id)
-    if(os.path.exists(folder_path) == False):
-        return 'Upload doesn\'t exists', 400
 
     path_pattern = os.path.join(folder_path, '*')
 
@@ -72,10 +101,11 @@ def stitch(upload_id):
 
     print(cmd)
 
-    subprocess.run([cmd], shell=True)
-
-    return '', 200
-
+    try:
+        subprocess.check_call([cmd], shell=True)
+        return '', 200
+    except subprocess.CalledProcessError:
+        return response_error('There was an error - command exited with non-zero code', 500)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
